28e6b99822cb03255e4ecf4a0ef8910170bdcf73
rabah babaci <4rabah@gmail.com>
2020-11-17 20:29:33 -0800

    create the products seeds

--- data/migrations/20201117190525_create-products.js
+++ data/migrations/20201117190525_create-products.js
@@ -2,20 +2,20 @@ exports.up = function (knex) {
   return knex.schema.createTable('products', function (table) {
     table.string('id').notNullable().unique().primary();
     table
-      .integer('profile_id')
-      .unsigned()
+      .string('profile_id')
       .notNullable()
       .references('id')
-      .inTable('products')
+      .inTable('profiles')
       .onDelete('CASCADE')
       .onUpdate('CASCADE');
 
-    table.string('title');
-    table.string('description');
-    table.string('category');
+    table.string('title').notNullable();
+    table.float('price').notNullable();
+    table.string('description').notNullable();
+    table.string('category').notNullable();
     table.string('brand');
-    table.string('condition');
-    table.string('delivery_method');
+    table.string('condition').notNullable();
+    table.string('delivery_method').notNullable();
     table.timestamps('created_at', { useTz: true });
   });
 };
--- data/seeds/001_profiles.js
+++ data/seeds/001_profiles.js
@@ -1,7 +1,15 @@
 const faker = require('faker');
 
+const ids = [
+  '00ulthapbErVUwVJy4x6',
+  '2j0epjkdhqo6wvkh0v7f',
+  'jhbgvwyejp81ds9jzuo9',
+  'bv8tg9jvp9t1461zr7x1',
+  'pv2864sfheem5j4o2vhs',
+];
+
 const profiles = [...new Array(5)].map((i, idx) => ({
-  id: idx === 0 ? '00ulthapbErVUwVJy4x6' : faker.random.alphaNumeric(20),
+  id: ids[idx],
   avatarUrl: faker.image.avatar(),
   email: idx === 0 ? 'llama001@maildrop.cc"' : faker.internet.email(),
   name:
