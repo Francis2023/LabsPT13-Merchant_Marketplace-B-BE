999aeb9894faa6f8ea1e703bd21bc9cd3f4451e1
rabah babaci <4rabah@gmail.com>
2020-11-20 12:14:25 -0800

    create the get user orders endpoint

--- api/orders/ordersRouter.js
+++ api/orders/ordersRouter.js
@@ -1,5 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const authRequired = require('../middleware/authRequired');
+const validateId = require('../middleware/validateId');
+
+const TABLE_NAME = 'orders';
+
+router.get('/:id', authRequired, validateId(TABLE_NAME), async (req, res) => {
+  res.status(200).json(req.order);
+});
 
 module.exports = router;
