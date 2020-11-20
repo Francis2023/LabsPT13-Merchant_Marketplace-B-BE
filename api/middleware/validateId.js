638331cdd88a04dd549c581926e63246b27cdac0
rabah babaci <4rabah@gmail.com>
2020-11-19 18:12:18 -0800

    use the validateId middleware on all requests

--- api/middleware/validateId.js
+++ api/middleware/validateId.js
@@ -1,12 +1,12 @@
 const getEntityName = require('../../helpers/getEntityName');
-const { getBy } = require('../globalDbModels');
+const { findBy } = require('../globalDbModels');
 
 const validateId = (tableName) => async (req, res, next) => {
   const entity = getEntityName(tableName);
 
   try {
-    const id = req.params.id;
-    const item = await getBy(tableName, { id });
+    const id = req.params.id || 0;
+    const item = await findBy(tableName, { id });
 
     if (item) {
       req[entity] = item;
@@ -15,6 +15,7 @@ const validateId = (tableName) => async (req, res, next) => {
       res.status(404).json({ message: `Could not find ${entity} ${id}` });
     }
   } catch (err) {
+    console.error(err);
     res.status(500).json({ message: err.message });
   }
 };
--- api/product/productRouter.js
+++ api/product/productRouter.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const authRequired = require('../middleware/authRequired');
+const validateId = require('../middleware/validateId');
 const router = express.Router();
 const Products = require('./productModel');
 const { findAll, findBy, update, remove } = require('../globalDbModels');
@@ -19,19 +20,8 @@ router.get('/', authRequired, (req, res) => {
 });
 
 // retrieve a product by the given :id
-router.get('/:id', authRequired, async (req, res) => {
-  const { id } = req.params;
-
-  try {
-    let product = await findBy(TABLE_NAME, { id });
-
-    product
-      ? res.status(200).json(product)
-      : res.status(404).json({ message: `Could not find product '${id}'` });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ message: err.message });
-  }
+router.get('/:id', authRequired, validateId(TABLE_NAME), async (req, res) => {
+  res.status(200).json(req.product);
 });
 
 // create a new product
@@ -56,49 +46,51 @@ router.post('/', authRequired, async (req, res) => {
 });
 
 router.put('/', authRequired, async (req, res) => {
-  const product = req.body;
+  const changes = req.body;
 
-  if (Object.keys(product).length > 0) {
-    const id = product.id || 0;
-    findBy(TABLE_NAME, { id })
-      .then(update(TABLE_NAME, product, { id }))
-      .then((updated) => {
+  if (Object.keys(changes).length > 0) {
+    const id = changes.id || 0;
+    try {
+      const product = await findBy(TABLE_NAME, { id });
+
+      if (product) {
+        const updated = await update(TABLE_NAME, changes, { id });
         res.status(200).json({ message: 'Product updated', product: updated });
-      })
-      .catch((err) => {
-        res.status(500).json({
-          message: `Could not update product '${id}'`,
-          error: err.message,
-        });
+      } else {
+        res
+          .status(404)
+          .json({ message: 'Could not find the specified product' });
+      }
+    } catch (err) {
+      res.status(500).json({
+        message: `Could not update product '${id}'`,
+        error: err.message,
       });
+    }
   } else {
     res.status(404).json({ message: 'Product info missing' });
   }
 });
 
-router.delete('/:id', authRequired, async (req, res) => {
-  const id = req.params.id;
+router.delete(
+  '/:id',
+  authRequired,
+  validateId(TABLE_NAME),
+  async (req, res) => {
+    try {
+      await remove(TABLE_NAME, { id: req.product.id });
 
-  try {
-    let product = await findBy(TABLE_NAME, { id });
-
-    if (Object.keys(product).length > 0) {
-      remove(TABLE_NAME, { id: product.id }).then(() => {
-        res
-          .status(200)
-          .json({ message: `Product '${id}' was deleted.`, product });
+      res.status(200).json({
+        message: `Product '${req.product.id}' was deleted.`,
+        product: req.product,
       });
-    } else {
-      res.status(404).json({
-        message: `Could not find product with ID: ${id}`,
+    } catch (err) {
+      res.status(500).json({
+        message: `Could not delete product with ID: ${req.product.id}`,
+        error: err.message,
       });
     }
-  } catch (err) {
-    res.status(500).json({
-      message: `Could not delete product with ID: ${id}`,
-      error: err.message,
-    });
   }
-});
+);
 
 module.exports = router;
