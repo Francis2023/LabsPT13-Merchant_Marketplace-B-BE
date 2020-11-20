84deaeaf8a74e1ff9817b44cb6706c1b4b2ad771
rabah babaci <4rabah@gmail.com>
2020-11-19 22:32:56 -0800

    remove unused functions to fix build errors

--- api/tags/tagsRouter.js
+++ api/tags/tagsRouter.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const authRequired = require('../middleware/authRequired');
 
-const { findAll, findBy, update, remove } = require('../globalDbModels');
+const { findAll } = require('../globalDbModels');
 
 const TABLE_NAME = 'tags';
 
