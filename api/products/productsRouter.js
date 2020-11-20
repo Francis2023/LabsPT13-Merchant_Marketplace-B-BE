ad769401401ddc2a91e30f30607671be5bb2bc69
rabah babaci <4rabah@gmail.com>
2020-11-19 23:29:58 -0800

    use the validate body middleware in all endpoints

--- api/products/productsRouter.js
+++ api/products/productsRouter.js
@@ -3,20 +3,20 @@ const router = express.Router();
 const Products = require('./productsModel');
 const authRequired = require('../middleware/authRequired');
 const validateId = require('../middleware/validateId');
+const validateBody = require('../middleware/validateBody');
 const { findAll, findBy, update, remove } = require('../globalDbModels');
 
 const TABLE_NAME = 'products';
 
 // retrieve all existing products
-router.get('/', authRequired, (req, res) => {
-  findAll(TABLE_NAME)
-    .then((products) => {
-      res.status(200).json(products);
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).json({ message: err.message });
-    });
+router.get('/', authRequired, async (req, res) => {
+  try {
+    const products = await findAll(TABLE_NAME);
+    res.status(200).json(products);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: err.message });
+  }
 });
 
 // retrieve a product by the given :id
@@ -25,50 +25,37 @@ router.get('/:id', authRequired, validateId(TABLE_NAME), async (req, res) => {
 });
 
 // create a new product
-router.post('/', authRequired, async (req, res) => {
-  let product = req.body;
+router.post('/', authRequired, validateBody, async (req, res) => {
+  const product = req.body;
 
-  if (Object.keys(product).length > 0) {
-    try {
-      let createdProduct = await Products.create(product);
-      res
-        .status(201)
-        .json({ message: 'Product created', product: createdProduct[0] });
-    } catch (err) {
-      console.error(err);
-      res.status(500).json({ message: err.message });
-    }
-  } else {
-    res.status(404).json({
-      message: 'Product info missing',
-    });
+  try {
+    const created = await Products.create(product);
+
+    res.status(201).json({ message: 'Product created', product: created[0] });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: err.message });
   }
 });
 
-router.put('/', authRequired, async (req, res) => {
+router.put('/', authRequired, validateBody, async (req, res) => {
   const changes = req.body;
+  const id = changes.id || 0;
 
-  if (Object.keys(changes).length > 0) {
-    const id = changes.id || 0;
-    try {
-      const product = await findBy(TABLE_NAME, { id });
+  try {
+    const product = await findBy(TABLE_NAME, { id });
 
-      if (product) {
-        const updated = await update(TABLE_NAME, changes, { id });
-        res.status(200).json({ message: 'Product updated', product: updated });
-      } else {
-        res
-          .status(404)
-          .json({ message: 'Could not find the specified product' });
-      }
-    } catch (err) {
-      res.status(500).json({
-        message: `Could not update product with ID: ${id}`,
-        error: err.message,
-      });
+    if (product) {
+      const updated = await update(TABLE_NAME, changes, { id });
+      res.status(200).json({ message: 'Product updated', product: updated });
+    } else {
+      res.status(404).json({ message: 'Could not find the specified product' });
     }
-  } else {
-    res.status(404).json({ message: 'Product is info missing' });
+  } catch (err) {
+    res.status(500).json({
+      message: `Could not update product with ID: ${id}`,
+      error: err.message,
+    });
   }
 });
 
--- api/tags/tagsRouter.js
+++ api/tags/tagsRouter.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const authRequired = require('../middleware/authRequired');
 const validateId = require('../middleware/validateId');
+const validateBody = require('../middleware/validateBody');
 
 const { findAll, findBy, update, remove } = require('../globalDbModels');
 
@@ -17,28 +18,24 @@ router.get('/', authRequired, async (req, res) => {
   }
 });
 
-router.put('/', authRequired, async (req, res) => {
+router.put('/', authRequired, validateBody, async (req, res) => {
   const changes = req.body;
+  const id = changes.id || 0;
 
-  if (Object.keys(changes).length > 0) {
-    const id = changes.id || 0;
-    try {
-      const tag = await findBy(TABLE_NAME, { id });
+  try {
+    const tag = await findBy(TABLE_NAME, { id });
 
-      if (tag) {
-        const updated = await update(TABLE_NAME, changes, { id });
-        res.status(200).json({ message: 'Tag updated', tag: updated });
-      } else {
-        res.status(404).json({ message: 'Could not find the specified tag' });
-      }
-    } catch (err) {
-      res.status(500).json({
-        message: `Could not update tag with ID: ${id}`,
-        error: err.message,
-      });
+    if (tag) {
+      const updated = await update(TABLE_NAME, changes, { id });
+      res.status(200).json({ message: 'Tag updated', tag: updated });
+    } else {
+      res.status(404).json({ message: 'Could not find the specified tag' });
     }
-  } else {
-    res.status(404).json({ message: 'Tag info is missing' });
+  } catch (err) {
+    res.status(500).json({
+      message: `Could not update tag with ID: ${id}`,
+      error: err.message,
+    });
   }
 });
 
