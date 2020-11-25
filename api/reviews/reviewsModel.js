b765d87c65722e9d8a09ae409963d6c53168a259
Bola Inc <fngafua2023@gmail.com>
2020-11-25 01:12:22 -0700

    fix errors in reviews backend

--- api/reviews/reviewsModel.js
+++ api/reviews/reviewsModel.js
@@ -1,10 +1,10 @@
 const db = require('../../data/db-config');
 
 const create = (review) => {
-    return db('reviews').insert(review).returning('*');
+  return db('reviews').insert(review).returning('*');
 };
 const findReview = (profile_id, product_id) => {
-    return db('users-carts').where({ profile_id, product_id }).first();
+  return db('users-carts').where({ profile_id, product_id }).first();
 };
-  
-module.exports = { create,findReview };
+
+module.exports = { create, findReview };
--- data/seeds/004_reviews.js
+++ data/seeds/004_reviews.js
@@ -1,9 +1,4 @@
-
-<<<<<<< HEAD
-
-=======
->>>>>>> 77695ae07c4af9bf10dad4ee770b65e612b11794
-exports.seed = function(knex) {
+exports.seed = function (knex) {
   // Deletes ALL existing entries
   return knex('reviews')
     .del()
@@ -11,7 +6,6 @@ exports.seed = function(knex) {
       // Inserts seed entries
       return knex('reviews').insert([
         {
-<<<<<<< HEAD
           profile_id: 'pv2864sfheem5j4o2vhs',
           product_id: 2,
           rate: 2,
@@ -32,25 +26,3 @@ exports.seed = function(knex) {
       ]);
     });
 };
-=======
-          product_id:1, 
-          profile_id: '00ulthapbErVUwVJy4x6',
-          rate: 3,
-          description: 'The product was good',
-        },
-        {
-          product_id:2, 
-          profile_id: '00ulthapbErVUwVJy4x6',
-          rate: 1,
-          description: 'The product was bad',
-        },
-        {
-          product_id:3, 
-          profile_id: '00ulthapbErVUwVJy4x6',
-          rate: 4,
-          description: 'very good product',
-        }
-      ]);
-    });
-};
->>>>>>> 77695ae07c4af9bf10dad4ee770b65e612b11794
